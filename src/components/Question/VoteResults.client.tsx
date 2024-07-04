bd37aa78e67acace653ae55fd40b3dfd8d223ccd
Justin <justin.gebert@redbull.com>
2024-07-04 13:17:24 +0200

    fix results bleeding over border

--- src/components/Question/VoteResults.client.tsx
+++ src/components/Question/VoteResults.client.tsx
@@ -29,23 +29,25 @@ const VoteResults = ({ question, avaiable}: any) => {
         {numOfVotes}
       </div>
       <div className="mb-10">
-      {results.map((result: any, index) => (
-        <Link key={index} href={`/dashboard/daily/resultsdetailed/${question._id}?returnTo=${question._id}`}>
-          <div className="bg-secondary p-1 my-2 rounded-md relative">
-            <div
-              className="bg-secondarydark h-10 rounded"
-              style={{ width: `${result.percentage}%` }}
-            ></div>
-            <div className="absolute inset-0 flex justify-between px-3 items-center">
-              <span>{result.option}</span>
-              <span>{result.percentage + " %"}</span>
+        {results.map((result: any, index) => (
+          <Link key={index} href={`/dashboard/daily/resultsdetailed/${question._id}?returnTo=${question._id}`}>
+            <div className="bg-secondary p-1 my-2 rounded-md relative">
+              <div
+                className="bg-secondarydark h-10 rounded"
+                style={{ width: `${result.percentage}%` }}
+              ></div>
+              <div className="absolute inset-0 flex justify-between px-3 items-center">
+                <span style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '80%' }}>
+                  {result.option}
+                </span>
+                <span style={{ whiteSpace: 'nowrap' }}>{result.percentage + " %"}</span>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))}
       </div>
       <Separator />
-      <ChatComponent questionId={question._id} avaiable={avaiable}/>
+      <ChatComponent questionId={question._id} avaiable={avaiable} />
     </div>
   );
 };
