e3bd16a6229c13a858d864ad7e67b53a2953d074
Justin <96232755+justingebert@users.noreply.github.com>
2024-07-30 21:15:25 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
                   headers: {
                     'Content-Type': 'application/json',
                   },
-                  body: JSON.stringify({ title: notification.title, body: notification.body }),
+                  body: JSON.stringify({ title: title, body: body }),
                   cache: 'no-cache',
                 });
               }}>
