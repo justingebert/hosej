43016fc494391b035a038959272df9654de3694b
Justin <96232755+justingebert@users.noreply.github.com>
2024-08-04 14:34:01 +0200

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
           <link rel="manifest" href="/manifest.json" id="manifest" />
           <link rel="icon" href="AppIcons/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-          <meta name="apple-mobile-web-app-capable" content="yes"/>
-          <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
+          {/* <meta name="apple-mobile-web-app-capable" content="yes"/>
+          <meta name="apple-mobile-web-app-status-bar-style" content="default"/> */}
           {/* <ThemeColorMeta /> */}
         </head>
         <body >
