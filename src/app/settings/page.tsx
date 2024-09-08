b1a4b82c0aae2374750479bd54571b539c181666
Justin <96232755+justingebert@users.noreply.github.com>
2024-09-08 20:04:53 +0200

    settigns styling

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Header from "@/components/ui/Header";
 import ThemeSelector from "@/components/ui/ThemeSelector";
@@ -12,101 +12,116 @@ import Loader from "@/components/ui/Loader";
 import { v4 as uuidv4 } from "uuid";
 import { useToast } from "@/hooks/use-toast";
 import { requestPermissionReturnToken } from "@/hooks/useFcmToken";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableFooter,
+} from "@/components/ui/table";
 
 export default function SettingsPage() {
-    const [userId, setUserId] = useState('');
-    const [notificationsEnabled, setNotificationsEnabled] = useState(false);
-    const [googleConnected, setGoogleConnected] = useState(false);
-    const { session, status, user } = useAuthRedirect();
-    const router = useRouter();
-    const { toast } = useToast();
+  const [userId, setUserId] = useState("");
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [googleConnected, setGoogleConnected] = useState(false);
+  const { session, status, user } = useAuthRedirect();
+  const router = useRouter();
+  const { toast } = useToast();
 
-    useEffect(() => {
-        if (status === "authenticated" && user) {
-            setUserId(user._id);
-            setGoogleConnected(!!user.googleId);
-            // Load the initial notification setting from local storage or server
-            const notificationSetting = localStorage.getItem('notificationsEnabled') === 'true';
-            setNotificationsEnabled(notificationSetting);
-        }
-    }, [status, user]);
+  useEffect(() => {
+    if (status === "authenticated" && user) {
+      setUserId(user._id);
+      setGoogleConnected(!!user.googleId);
+      // Load the initial notification setting from local storage or server
+      const notificationSetting =
+        localStorage.getItem("notificationsEnabled") === "true";
+      setNotificationsEnabled(notificationSetting);
+    }
+  }, [status, user]);
 
-    if (status === "loading" || !user) return <Loader loading={true} />;
+  if (status === "loading" || !user) return <Loader loading={true} />;
 
-    const handleGoogleConnect = async () => {
-        await signIn('google', { callbackUrl: `/connectgoogle` })
+  const handleGoogleConnect = async () => {
+    await signIn("google", { callbackUrl: `/connectgoogle` });
+  };
+
+  const handlegoogleDisconnect = async () => {
+    const deviceId = uuidv4();
+
+    await localStorage.setItem("deviceId", deviceId);
+
+    if (deviceId) {
+      const confirmation = window.confirm(
+        "If you did not Connect you Google or Mail all data will be lost if you log out. Do you wish to continue?"
+      );
+      if (!confirmation) {
+        return;
+      }
     }
 
-    const handlegoogleDisconnect = async () => {
-        const deviceId = uuidv4();
+    await fetch("/api/google/disconnect", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userId: user._id, deviceId: deviceId }),
+    });
 
-        await localStorage.setItem('deviceId', deviceId);
+    await signIn("credentials", { redirect: false, deviceId: deviceId });
+    toast({ title: "Google account unlinked!" });
+  };
 
-        if (deviceId) {
-            const confirmation = window.confirm("If you did not Connect you Google or Mail all data will be lost if you log out. Do you wish to continue?");
-            if (!confirmation) {
-                return;
-            }
-        }
+  const handleLogout = async () => {
+    const deviceId = localStorage.getItem("deviceId");
 
-        await fetch('/api/google/disconnect', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ userId: user._id, deviceId: deviceId }),
+    if (deviceId) {
+      const confirmation = window.confirm(
+        "If you did not Connect you Google or Mail all data will be lost if you log out. Do you wish to continue?"
+      );
+      if (!confirmation) {
+        return;
+      }
+    }
+
+    localStorage.removeItem("deviceId");
+    await signOut();
+    router.push("/");
+  };
+
+  const handleNotificationToggle = async () => {
+    if (!notificationsEnabled) {
+      // Request permission and get FCM token
+      const token = await requestPermissionReturnToken();
+      if (token) {
+        // Send the token to the server
+        await fetch(`/api/users/${userId}/register-push`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token }),
         });
-
-        await signIn('credentials', {redirect: false, deviceId: deviceId})
-        toast({ title: "Google account unlinked!" });
+        console.log("FCM token registered successfully.");
+        await localStorage.setItem("lastSentFcmToken", token);
+      }
+    } else {
+      const token = localStorage.getItem("lastSentFcmToken");
+      if (token) {
+        await fetch(`/api/users/${userId}/unregister-push`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token }),
+        });
+        console.log("FCM token unregistered successfully.");
+        localStorage.removeItem("lastSentFcmToken");
+      }
     }
 
-    const handleLogout = async () => {
-        const deviceId = localStorage.getItem("deviceId");
+    const newValue = !notificationsEnabled;
+    setNotificationsEnabled(newValue);
+    localStorage.setItem("notificationsEnabled", newValue.toString());
+  };
 
-        if (deviceId) {
-            const confirmation = window.confirm("If you did not Connect you Google or Mail all data will be lost if you log out. Do you wish to continue?");
-            if (!confirmation) {
-                return;
-            }
-        }
-
-        localStorage.removeItem("deviceId");
-        await signOut();
-        router.push("/");
-    };
-
-    const handleNotificationToggle = async () => {
-        if (!notificationsEnabled) {
-            // Request permission and get FCM token
-            const token = await requestPermissionReturnToken();
-            if (token) {
-                // Send the token to the server
-                await fetch(`/api/users/${userId}/register-push`, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ token }),
-                });
-                console.log('FCM token registered successfully.');
-                await localStorage.setItem('lastSentFcmToken', token);
-            }
-        } else {
-            const token = localStorage.getItem('lastSentFcmToken');
-            if (token) {
-                await fetch(`/api/users/${userId}/unregister-push`, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ token }),
-                });
-                console.log('FCM token unregistered successfully.');
-                localStorage.removeItem('lastSentFcmToken');
-            }
-        }
-
-        const newValue = !notificationsEnabled;
-        setNotificationsEnabled(newValue);
-        localStorage.setItem('notificationsEnabled', newValue.toString());
-    };
-
-/*     const handleReportBug = () => {
+  /*     const handleReportBug = () => {
         //router.push("/report-bug"); // Assuming you have a route for reporting bugs
     };
 
@@ -115,49 +130,88 @@ export default function SettingsPage() {
     };
  */
 
-    const formattedDate = new Date((user as any).createdAt).toLocaleDateString(undefined, {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-    });
+  const formattedDate = new Date((user as any).createdAt).toLocaleDateString(
+    undefined,
+    {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    }
+  );
 
-    return (
-        <>
-            <Header href={`/groups/`} title="Settings" rightComponent={<ThemeSelector />} />
-            <div className="mt-10">
-                <div className="mb-4">
-                    <div className="flex items-center justify-between">
-                        <span>Enable Notifications</span>
-                        <Switch checked={notificationsEnabled} onCheckedChange={handleNotificationToggle} />
-                    </div>
-                </div>
-                <h2 className="text-xl font-bold mb-4">User Settings</h2>
-                <div className="mb-4">
-                    <div>User ID: {user._id}</div>
-                    <div>Username: {user.username}</div>
-                    <div>Joined: {formattedDate}</div>
-                    <div>Google Connected: {googleConnected ? 'Yes' : 'No'}</div>
-                    {user.deviceId && <div>DeviceID: {user.deviceId}</div>}
-                </div>
-                
-                <div className="flex flex-col justify-center mt-10 gap-5">
-                    {!googleConnected ?  
-                    <Button onClick={handleGoogleConnect} className="w-full" >Connect with Google</Button> : 
-                    <Button onClick={handlegoogleDisconnect} className="w-full" variant={"destructive"}>Disconnect Google</Button> }
-                    <Button onClick={handleLogout} variant="destructive" className="w-full">
-                        Logout
-                    </Button>
-                </div>
-                
-                {/* <div className="flex flex-row justify-evenly mt-10 gap-5">
-                    <Button onClick={handleReportBug} className="w-full" variant={"outline"}>
-                        Report a Bug
-                    </Button>
-                    <Button onClick={handleFeatureRequest} className="w-full" variant={"outline"}>
-                        Feature Request
-                    </Button>
-                </div> */}
-            </div>
-        </>
-    );
+  return (
+    <div className="flex flex-col h-[100dvh]">
+  <Header
+    href={`/groups/`}
+    title="Settings"
+    rightComponent={<ThemeSelector />}
+  />
+  <div className="mt-10 flex-grow">
+    <div className="mb-4">
+      <div className="flex items-center justify-between">
+        <span>Enable Notifications</span>
+        <Switch
+          checked={notificationsEnabled}
+          onCheckedChange={handleNotificationToggle}
+        />
+      </div>
+    </div>
+    <h2 className="text-xl font-bold mb-4">User Data:</h2>
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">User ID</TableCell>
+          <TableCell>{user._id}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Username</TableCell>
+          <TableCell>{user.username}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Joined</TableCell>
+          <TableCell>{formattedDate}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Google Connected</TableCell>
+          <TableCell>{googleConnected ? "Yes" : "No"}</TableCell>
+        </TableRow>
+        {user.deviceId && (
+          <TableRow>
+            <TableCell className="font-medium">Device ID</TableCell>
+            <TableCell>{user.deviceId}</TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+
+    <div className="flex flex-col justify-center mt-10 gap-5">
+      {!googleConnected ? (
+        <Button onClick={handleGoogleConnect} className="w-full">
+          Connect with Google
+        </Button>
+      ) : (
+        <Button
+          onClick={handlegoogleDisconnect}
+          className="w-full"
+          variant={"destructive"}
+        >
+          Disconnect Google
+        </Button>
+      )}
+    </div>
+  </div>
+
+  {/* Logout button at the bottom */}
+  <div className="mt-auto mb-14">
+    <Button
+      onClick={handleLogout}
+      variant="destructive"
+      className="w-full"
+    >
+      Logout
+    </Button>
+  </div>
+</div>
+
+  );
 }
