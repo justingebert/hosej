d3284767a6dc16a93d3b49de30b5b2828a54091c
Justin <justin.gebert.berlin@gmail.com>
2024-10-21 15:54:57 +0200

    refactor google routes

--- src/app/api/auth/google/connect/route.ts
+++ src/app/api/auth/google/connect/route.ts
@@ -6,25 +6,20 @@ export async function POST(req: Request) {
   try {
     const body = await req.json(); 
     const { deviceId, googleUserId } = body;
-
     if (!deviceId) {
-      return NextResponse.json({ success: false, message: 'No deviceId provided' }, { status: 400 });
+      return NextResponse.json({message: 'No deviceId provided' }, { status: 400 });
     }
 
     await dbConnect();
 
-
     const googleUser = await User.findById(googleUserId);
-
     if (!googleUser) {
-      return NextResponse.json({ success: false, message: 'Google user not found' }, { status: 404 });
+      return NextResponse.json({ message: 'Google user not found' }, { status: 404 });
     }
 
-
     const deviceUser = await User.findOne({ deviceId });
-
     if (!deviceUser) {
-      return NextResponse.json({ success: false, message: 'User with deviceId not found' }, { status: 404 });
+      return NextResponse.json({message: 'User with deviceId not found' }, { status: 404 });
     }
 
     const googleId = googleUser.googleId;
@@ -34,10 +29,10 @@ export async function POST(req: Request) {
     deviceUser.deviceId = undefined;
     await deviceUser.save();
     
-    return NextResponse.json({ success: true, message: 'Google account linked successfully.' }, { status: 200 });
+    return NextResponse.json({ message: 'Google account linked successfully.' }, { status: 200 });
 
   } catch (error) {
     console.error('Error merging accounts:', error);
-    return NextResponse.json({ success: false, message: 'Server error' }, { status: 500 });
+    return NextResponse.json({message: 'Internal Server Error' }, { status: 500 });
   }
 }
--- src/app/api/auth/google/disconnect/route.ts
+++ src/app/api/auth/google/disconnect/route.ts
@@ -6,27 +6,25 @@ export async function POST(req: Request) {
   try {
     const body = await req.json(); 
     const { deviceId, userId } = body;
-
     if (!deviceId) {
-      return NextResponse.json({ success: false, message: 'No deviceId provided' }, { status: 400 });
+      return NextResponse.json({message: 'No deviceId provided' }, { status: 400 });
     }
 
     await dbConnect();
 
     const googleUser = await User.findById(userId);
-
     if (!googleUser) {
-      return NextResponse.json({ success: false, message: 'Google user not found' }, { status: 404 });
+      return NextResponse.json({ message: 'Google user not found' }, { status: 404 });
     }
 
     googleUser.deviceId = deviceId;
     googleUser.googleId = undefined;
     await googleUser.save();
     
-    return NextResponse.json({ success: true, message: 'Google account successfully unlinked.' }, { status: 200 });
+    return NextResponse.json({ message: 'Google account successfully unlinked.' }, { status: 200 });
 
   } catch (error) {
     console.error('Error merging accounts:', error);
-    return NextResponse.json({ success: false, message: 'Server error' }, { status: 500 });
+    return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
   }
 }
