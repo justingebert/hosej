398142751ef0673f1d0698da99a84a0cd9cff5a6
Justin <justin.gebert.berlin@gmail.com>
2024-11-12 10:13:31 +0100

    refactor home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { FaGoogle } from "react-icons/fa";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Input } from "@/components/ui/input";
@@ -14,116 +15,76 @@ import { HoseJLoader } from "../components/ui/custom/HoseJLoader";
 function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
-  const [userName, setUserName] = useState("");
-  const [loading, setLoading] = useState(true); 
-  const [isRedirecting, setIsRedirecting] = useState(false); 
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/groups";
   const { toast } = useToast();
+  const [userName, setUserName] = useState("");
+  const [loading, setLoading] = useState(true); 
+  const [isRedirecting, setIsRedirecting] = useState(false); 
 
+  const handleSignIn = useCallback(async (provider:string, options = {}) => {
+    try {
+      const result = await signIn(provider, { ...options, redirect: false });
+      if (result?.ok) {
+        setIsRedirecting(true);
+        router.push(callbackUrl);
+      } else {
+        console.error(`${provider} sign-in failed:`, result?.error);
+      }
+    } catch (error) {
+      console.error(`Error during ${provider} sign-in:`, error);
+      toast({ title: "Failed to sign in!", variant: "destructive"});
+    } finally {
+      setLoading(false);
+    }
+  }, [callbackUrl, router]);
+
+  const handleStartWithoutAccount = async () => {
+    if (!userName) {
+      toast({ title: "Please Enter your name!", variant: "destructive" });
+      return;
+    }
+
+    let deviceId = localStorage.getItem("deviceId");
+    if (!deviceId) {
+      deviceId = uuidv4();
+      await createUserByDeviceId(deviceId, userName);
+    }
+    await handleSignIn("credentials", { deviceId });
+  };
+
+  const createUserByDeviceId = async (deviceId:string, userName:string) => {
+    try {
+      const response = await fetch("/api/users/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ deviceId, userName }),
+      });
+
+      if (response.ok) {
+        await localStorage.setItem("deviceId", deviceId);
+        console.log("User created successfully");
+      } else {
+        console.error("Failed to create user:", await response.text());
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
 
   useEffect(() => {
-    if (status === "loading" || isRedirecting) {
-      return; 
-    }
+    if (status === "loading" || isRedirecting) return;
 
     if (session) {
       setIsRedirecting(true);
       router.push(callbackUrl);
-      return;
-    }
-
-    const deviceId = localStorage.getItem("deviceId");
-    if (deviceId) {
-      signIn('credentials', {
-        redirect: false,
-        deviceId: deviceId,
-      }).then(result => {
-        if (result?.ok) {
-          setIsRedirecting(true); 
-          router.push('/groups');
-        } else {
-          console.error('Device ID authentication failed:', result?.error); // Log the full result object for more context
-          setLoading(false); // Stop loading if authentication fails
-        }
-      }).catch(error => {
-        console.error('Error during device ID authentication:', error);
-        setLoading(false); // Stop loading on error
-      });
     } else {
-      setLoading(false); // Stop loading if no device ID found
+      const deviceId = localStorage.getItem("deviceId");
+      if (deviceId) handleSignIn("credentials", { deviceId });
+      else setLoading(false);
     }
-  }, [session, router, callbackUrl, status, isRedirecting]);
+  }, [session, status, isRedirecting, handleSignIn, router, callbackUrl]);
 
-  const handleGoogleSignIn = () => {
-      signIn('google', { callbackUrl: callbackUrl }).catch(error => {
-        console.error('Google sign-in error:', error);
-      });
-  };
-
-  
-  const handleEmailSignIn = () => {
-    signIn('google', { callbackUrl: '/groups' }).catch(error => {
-      console.error('Google sign-in error:', error);
-    });
-};
-
-  const createUserByDeviceId = async (userName: string) => {
-    const deviceId = uuidv4();
-
-    try {
-      console.log(deviceId, userName);
-      const response = await fetch('/api/users/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ deviceId, userName }),
-      });
-      console.log(response);
-      if (response.ok) {;
-        await localStorage.setItem('deviceId', deviceId);
-        console.log('User created successfully');
-      } else {
-        console.error('Failed to create user:', await response.text());
-      }
-    } catch (error) {
-      console.error('Error creating user:', error);
-    }
-  };
-
-  const handleStartWithoutAccount = async () => {
-    if(!userName) {
-      toast({ title: "Please Enter your name!" });
-      return;
-    }
-    try {
-      let deviceId = await localStorage.getItem("deviceId");
-      if (!deviceId) {
-        await createUserByDeviceId(userName);
-      }
-      deviceId = await localStorage.getItem("deviceId");
-      
-      const result = await signIn('credentials', {
-          redirect: false,
-          deviceId: deviceId,
-          callbackUrl: callbackUrl,
-        });
-  
-        if (result?.ok) {
-          setIsRedirecting(true); 
-          router.push(callbackUrl); 
-        } else {
-          console.error('Device ID authentication failed:', result?.error);
-        }
-      
-      
-      setLoading(false);
-    } catch (error) {
-      console.error('Error during device ID authentication:', error);
-      setLoading(false);
-    }
-  };
 
   if (loading || status === "loading" || isRedirecting) {
     return <HoseJLoader />;
@@ -131,11 +92,7 @@ function Home() {
 
   return (
     <div className="flex flex-col justify-between min-h-screen ">
-      <header className="text-center p-6">
-        <Link href={"/deviceauth"}>
-          <h1 className="text-4xl font-bold">HoseJ</h1>
-        </Link>
-      </header>
+      <Header />
 
       <main className="flex flex-col items-center justify-center flex-grow space-y-6">
         <Input
@@ -147,36 +104,12 @@ function Home() {
           className="w-full max-w-sm text-center"
         />
 
-        <div className="space-y-4 w-full max-w-sm">
-        <Button
-            onClick={handleStartWithoutAccount}
-            variant="secondary"
-            className="w-full"
-          >
-            Start without Account
-          </Button>
-          <Button onClick={handleGoogleSignIn} className="w-full">
-            Continue with Google
-          </Button>
-{/*           <Button onClick={handleGoogleSignIn} className="w-full" disabled={true} >
-            Continue with Email
-          </Button> */}
-        </div>
+        <SignInButtons
+          onStartWithoutAccount={handleStartWithoutAccount}
+          onGoogleSignIn={() => handleSignIn("google", { callbackUrl })}
+        />
       </main>
-
-      <footer className="text-center p-4">
-        <p className="text-sm text-muted">
-          By continuing, you agree to our{" "}
-          <a href="/terms" className="underline">
-            Terms of Service
-          </a>{" "}
-          and{" "}
-          <a href="/privacy" className="underline">
-            Privacy Policy
-          </a>
-          .
-        </p>
-      </footer>
+      <Footer/>
     </div>
   );
 }
@@ -187,4 +120,43 @@ export default function HomeWithSuspense(){
       <Home />
     </Suspense>
   );
+}
+
+function SignInButtons({ onStartWithoutAccount, onGoogleSignIn }:{onStartWithoutAccount:()=>void, onGoogleSignIn:()=>void}) {
+  return (
+    <div className="space-y-4 w-full max-w-sm">
+      <Button onClick={onStartWithoutAccount} variant="secondary" className="w-full">
+        Start without Account
+      </Button>
+      <Button onClick={onGoogleSignIn} className="w-full">
+        <FaGoogle className="mr-2"/>
+        Continue with Google
+      </Button>
+    </div>
+  );
+}
+
+
+function Footer() {
+  return <footer className="text-center p-4">
+    <p className="text-sm text-muted">
+      By continuing, you agree to our{" "}
+      <a href="/terms" className="underline">
+        Terms of Service
+      </a>{" "}
+      and{" "}
+      <a href="/privacy" className="underline">
+        Privacy Policy
+      </a>
+      .
+    </p>
+  </footer>;
+}
+
+function Header() {
+  return <header className="text-center p-6">
+    <Link href={"/deviceauth"}>
+      <h1 className="text-4xl font-bold">HoseJ</h1>
+    </Link>
+  </header>;
 }
