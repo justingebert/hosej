0d16c3a55399f2adcfa685b5cf05d85f56e64bcc
Justin <justin.gebert.berlin@gmail.com>
2024-11-12 14:16:07 +0100

    refactor settings

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -2,17 +2,24 @@ import Header from '@/components/ui/custom/Header';
 import cat from '/public/cat.jpg';
 import Image from 'next/image'
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default function HelpPage() {
     return (
         <>
-            <Header href={`/groups`}/>
+            <Header href={`/groups`} title={"Help"}/>
             <Image src={cat} alt="cat" className="rounded-lg" />
-            <div>
-                <Link href={"/terms"}>Terms of Service</Link>    
-            </div>
-            <div>
-                <Link href={"/privacy"}>Privacy Policy</Link>
+            <div className='flex flex-row justify-between my-6'>
+                <Link href={"/terms"}>
+                    <Button variant='secondary'>
+                    Terms of Service
+                    </Button>
+                    </Link>    
+                <Link href={"/privacy"}>
+                    <Button variant='secondary'>
+                    Privacy Policy
+                    </Button>
+                </Link>
             </div>
         </>
     );
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -8,81 +8,62 @@ import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 import { useState, useEffect } from "react";
 import { signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import SpinningLoader from "@/components/ui/custom/SpinningLoader";
 import { v4 as uuidv4 } from "uuid";
 import { useToast } from "@/hooks/use-toast";
 import { requestPermissionReturnToken } from "@/hooks/useFcmToken";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableHead,
-  TableHeader,
   TableRow,
-  TableFooter,
 } from "@/components/ui/table";
+import { FaGoogle } from "react-icons/fa";
+import { IUser } from "@/db/models/user";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function SettingsPage() {
-  const [userId, setUserId] = useState("");
-  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
-  const [googleConnected, setGoogleConnected] = useState(false);
-  const { session, status, user } = useAuthRedirect();
+  const { status, user } = useAuthRedirect();
   const router = useRouter();
   const { toast } = useToast();
 
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [googleConnected, setGoogleConnected] = useState(false)
+
   useEffect(() => {
     if (status === "authenticated" && user) {
-      setUserId(user._id);
       setGoogleConnected(!!user.googleId);
-      // Load the initial notification setting from local storage or server
-      const notificationSetting =
-        localStorage.getItem("notificationsEnabled") === "true" || !!user.fcmToken;
-      setNotificationsEnabled(notificationSetting);
+    const notificationSetting =
+      localStorage.getItem("notificationsEnabled") === "true" || !!user.fcmToken;
+    setNotificationsEnabled(notificationSetting);
     }
   }, [status, user]);
 
-  if (status === "loading" || !user) return <SpinningLoader loading={true} />;
-
-  const handleGoogleConnect = async () => {
-    await signIn("google", { callbackUrl: `/connectgoogle` });
-  };
+  if (status === "loading" || !user) return <SettingsSkeleton />;
 
   const handlegoogleDisconnect = async () => {
     const deviceId = uuidv4();
+    localStorage.setItem("deviceId", deviceId);
 
-    await localStorage.setItem("deviceId", deviceId);
-
-    if (deviceId) {
-      const confirmation = window.confirm(
-        "If you did not Connect you Google or Mail all data will be lost if you log out. Do you wish to continue?"
-      );
-      if (!confirmation) {
-        return;
-      }
-    }
+    const confirmation = window.confirm(
+      "If you did not Connect your Google or Mail, all data will be lost if you log out. Do you wish to continue?"
+    );
+    if (!confirmation) return;
 
     await fetch("/api/auth/google/disconnect", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userId: user._id, deviceId: deviceId }),
+      body: JSON.stringify({ userId: user._id, deviceId }),
     });
 
-    await signIn("credentials", { redirect: false, deviceId: deviceId });
+    await signIn("credentials", { redirect: false, deviceId });
     toast({ title: "Google account unlinked!" });
   };
 
   const handleLogout = async () => {
-    const deviceId = localStorage.getItem("deviceId");
-
-    if (deviceId) {
-      const confirmation = window.confirm(
-        "If you did not Connect you Google or Mail all data will be lost if you log out. Do you wish to continue?"
-      );
-      if (!confirmation) {
-        return;
-      }
-    }
+    const confirmation = window.confirm(
+      "If you did not Connect your Google or Mail, all data will be lost if you log out. Do you wish to continue?"
+    );
+    if (!confirmation) return;
 
     localStorage.removeItem("deviceId");
     await signOut();
@@ -91,22 +72,20 @@ export default function SettingsPage() {
 
   const handleNotificationToggle = async () => {
     if (!notificationsEnabled) {
-      // Request permission and get FCM token
       const token = await requestPermissionReturnToken();
       if (token) {
-        // Send the token to the server
-        await fetch(`/api/users/${userId}/register-push`, {
+        await fetch(`/api/users/${user._id}/register-push`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ token }),
         });
         console.log("FCM token registered successfully.");
-        await localStorage.setItem("lastSentFcmToken", token);
+        localStorage.setItem("lastSentFcmToken", token);
       }
     } else {
       const token = localStorage.getItem("lastSentFcmToken");
       if (token) {
-        await fetch(`/api/users/${userId}/unregister-push`, {
+        await fetch(`/api/users/${user._id}/unregister-push`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ token }),
@@ -116,104 +95,102 @@ export default function SettingsPage() {
       }
     }
 
-    const newValue = !notificationsEnabled;
-    setNotificationsEnabled(newValue);
-    localStorage.setItem("notificationsEnabled", newValue.toString());
+    setNotificationsEnabled(!notificationsEnabled);
+    localStorage.setItem("notificationsEnabled", notificationsEnabled.toString());
   };
-
-  /*     const handleReportBug = () => {
-        //router.push("/report-bug"); // Assuming you have a route for reporting bugs
-    };
-
-    const handleFeatureRequest = () => {
-        //router.push("/feature-request"); // Assuming you have a route for feature requests
-    };
- */
-
-  const formattedDate = new Date((user as any).createdAt).toLocaleDateString(
-    undefined,
-    {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-    }
-  );
 
   return (
     <div className="flex flex-col h-[100dvh]">
-  <Header
-    href={`/groups/`}
-    title="Settings"
-    rightComponent={<ThemeSelector />}
-  />
-  <div className="mt-10 flex-grow">
-    <div className="mb-4">
-      <div className="flex items-center justify-between">
-        <span>Enable Notifications</span>
-        <Switch
-          checked={notificationsEnabled}
-          onCheckedChange={handleNotificationToggle}
+      <Header href={`/groups/`} title="Settings" rightComponent={<ThemeSelector />} />
+      <div className="flex-grow mt-4">
+          <div className="flex items-center justify-between mb-4">
+            <span>Notifications</span>
+            <Switch checked={notificationsEnabled} onCheckedChange={handleNotificationToggle} />
+          </div>
+
+        <UserDataTable user={user} googleConnected={googleConnected} />
+        <GoogleConnectButton
+          googleConnected={googleConnected}
+          onDisconnect={handlegoogleDisconnect}
+          className="mt-4"
         />
       </div>
-    </div>
-    <h2 className="text-xl font-bold mb-4">User Data:</h2>
-    <Table>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">User ID</TableCell>
-          <TableCell>{user._id}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-medium">Username</TableCell>
-          <TableCell>{user.username}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-medium">Joined</TableCell>
-          <TableCell>{formattedDate}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-medium">Google Connected</TableCell>
-          <TableCell>{googleConnected ? "Yes" : "No"}</TableCell>
-        </TableRow>
-        {user.deviceId && (
-          <TableRow>
-            <TableCell className="font-medium">Device ID</TableCell>
-            <TableCell>{user.deviceId}</TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-
-    <div className="flex flex-col justify-center mt-10 gap-5">
-      {!googleConnected ? (
-        <div>
-        <Button onClick={handleGoogleConnect} className="w-full">
-          Connect with Google
+      <div className="mt-auto mb-14">
+        <Button onClick={handleLogout} variant="destructive" className="w-full">
+          Logout
         </Button>
-        </div>
-      ) : (
-        <Button
-          onClick={handlegoogleDisconnect}
-          className="w-full"
-          variant={"destructive"}
-        >
+      </div>
+    </div>
+  );
+}
+
+function UserDataTable({ user, googleConnected }: {user: IUser, googleConnected: boolean}) {
+  const formattedDate = new Date(user.createdAt).toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+
+  return (
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">User ID</TableCell>
+            <TableCell>{user._id}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Username</TableCell>
+            <TableCell>{user.username}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Joined</TableCell>
+            <TableCell>{formattedDate}</TableCell>
+          </TableRow>
+          {user.deviceId && (
+            <TableRow>
+              <TableCell className="font-medium">Device ID</TableCell>
+              <TableCell>{user.deviceId}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+  );
+}
+
+function GoogleConnectButton({ googleConnected, onDisconnect, className }:{googleConnected:boolean, onDisconnect:()=>void, className:string}) {
+    return (
+      <div className={className}>
+        {googleConnected ? (
+        <Button onClick={onDisconnect} className="w-full" variant="destructive">
+          <FaGoogle className="mr-2" />
           Disconnect Google
+        </Button>
+      ) : (
+        <Button onClick={async ()  => {await signIn("google", { callbackUrl: `/connectgoogle` });}} className="w-full">
+          <FaGoogle className="mr-2" />
+          Connect with Google
         </Button>
       )}
     </div>
-  </div>
+  )
+}
 
-  {/* Logout button at the bottom */}
-  <div className="mt-auto mb-14">
-    <Button
-      onClick={handleLogout}
-      variant="destructive"
-      className="w-full"
-    >
-      Logout
-    </Button>
-  </div>
-</div>
-
+function SettingsSkeleton() {
+  return (
+    <div className="flex flex-col h-[100dvh]">
+      <Header href={`/groups/`} title="Settings" rightComponent={<ThemeSelector />} />
+      <div className="flex-grow mt-4 space-y-4">
+        <div className="flex items-center justify-between mb-4">
+          <span>Notifications</span>
+          <Skeleton className="h-6 w-10" />
+        </div>
+        {[...Array(4)].map((_, i) => (
+              <Skeleton key={i} className="h-10" />
+        ))}
+      </div>
+      <div className="mt-auto mb-14">
+        <Skeleton className="h-12 w-full" />
+      </div>
+    </div>
   );
 }
