dc90ecdd47828afe3c4a92cba812c32f38817200
Justin <justin.gebert.berlin@gmail.com>
2024-11-12 13:18:07 +0100

    refactor groups page

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,116 +1,70 @@
-"use client"
-
-import { useState, useEffect } from "react";
+"use client";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Settings, CircleHelp } from 'lucide-react';
+import { Settings, CircleHelp, Copy } from "lucide-react";
 import {
   Card,
   CardContent,
   CardDescription,
   CardTitle,
 } from "@/components/ui/card";
-
 import { IGroup } from "@/db/models/Group";
-import { Copy } from "lucide-react";
-import { useAuthRedirect } from "@/hooks/useAuthRedirect";
-import { motion } from 'framer-motion';
 import { CreateGroupDrawer, JoinGroupDrawer } from "@/components/Group/groupdrawers";
-
+import { Skeleton } from "@/components/ui/skeleton";
+import useSWR, { mutate } from "swr";
+import fetcher from "@/lib/fetcher";
+import { useToast } from "@/hooks/use-toast";
 
 export default function GroupsPage() {
-  const { session, status, user } = useAuthRedirect();
-  const [groups, setGroups] = useState<IGroup[]>([]);
+  const { toast } = useToast();
   const router = useRouter();
 
-  useEffect(() => {
-    const fetchGroups = async () => {
-      if (status === "loading") return; // Do nothing while loading
-
-      const res = await fetch('/api/users/groups', {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-        //body: JSON.stringify({ user: user }),
+  const createGroup = async (groupName: string) => {
+    try {
+      const res = await fetch("/api/groups/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: groupName }),
       });
 
       if (res.ok) {
-        const groupsData = await res.json();
-        setGroups(groupsData.groups);
+        const newGroup = await res.json();
+        mutate("/api/users/groups", (data) => ({ groups: [...data.groups, newGroup] }), false);
       } else {
-        console.error('Failed to fetch groups');
+        console.error("Failed to create group");
+        toast({ title: "Failed to create group!", variant: "destructive" });
       }
-    };
-
-    fetchGroups();
-  }, [session, status, user]);
-
-  const createGroup = async (groupName: string) => {
-    const res = await fetch('/api/groups/create', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name: groupName}),
-    });
-
-    if (res.ok) {
-      const newGroup = await res.json();
-      setGroups((prevGroups) => [...prevGroups, newGroup]);
-    } else {
-      console.error('Failed to create group');
+    } catch (error) {
+      console.error("Failed to create group: ", error);
+      toast({ title: "Failed to create group!", variant: "destructive" });
     }
   };
 
-  const copyToClipboard = (text: string) => {
+  function copyToClipboard(text: string) {
     navigator.clipboard.writeText(text).then(() => {
-      alert("Link copied to clipboard!");
+      toast({title: "Link copied to clipboard!"});
     }).catch((err) => {
       console.error("Failed to copy: ", err);
     });
-  };
-
-
-  if (status === "loading") {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <motion.h1
-          className="text-4xl font-bold text-center relative"
-          style={{
-            backgroundImage: "linear-gradient(90deg, var(--shine-color) 0%, var(--shine-highlight) 50%, var(--shine-color) 100%)",
-            backgroundSize: "200% 100%",
-            backgroundClip: "text",
-            WebkitBackgroundClip: "text",
-            color: "transparent",
-          }}
-          animate={{
-            backgroundPosition: ["-100% 0", "100% 0"],
-          }}
-          transition={{
-            duration: 1.5,
-            ease: "easeInOut",
-            repeat: Infinity,
-          }}
-        >
-          HoseJ
-        </motion.h1>
-      </div>
-    );
   }
-  
 
   return (
     <div className="relative min-h-screen flex flex-col">
-    {/* Header */}
-    <div className="flex justify-between items-center w-full">
-      <Button variant="outline" size="icon" onClick={() => { router.push(`/help`)}}>
-        <CircleHelp/>
-      </Button>
-      <h1 className="text-4xl font-bold">HoseJ</h1>
-      <Button variant="outline" size="icon" onClick={() => { router.push(`/settings`)}}>
-        <Settings/>
-      </Button>
+      <Header router={router} />
+      <GroupsList router={router} copyFn={copyToClipboard}/>
+      <Footer createGroup={createGroup} />
     </div>
+  );
+}
 
-    {/* Group List */}
-    <div className="flex-grow overflow-y-auto py-6">
+function GroupsList({ router, copyFn }: { router: ReturnType<typeof useRouter>, copyFn: (text: string) => void }) {
+  const { data, isLoading } = useSWR<{ groups: IGroup[] }>("/api/users/groups", fetcher, { });
+  const groups = data?.groups || [];
+
+  return (
+    <>
+    {isLoading ? <GroupListSkeleton/>: 
+      <div className="flex-grow overflow-y-auto py-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-24">
         {groups.map((group) => (
           <Card
@@ -128,7 +82,7 @@ export default function GroupsPage() {
                 size="icon"
                 onClick={(e) => {
                   e.stopPropagation();
-                  copyToClipboard(`${group._id}`);
+                  copyFn(`${group._id}`);
                 }}
               >
                 <Copy className="w-4 h-4" />
@@ -138,14 +92,53 @@ export default function GroupsPage() {
         ))}
       </div>
     </div>
+    }
+    </>
+  );
+}
 
+function GroupListSkeleton() {
+  return (
+    <div className="flex-grow overflow-y-auto py-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-24">
+        {[...Array(6)].map((_, i) => (
+          <Card key={i} className="cursor-pointer">
+            <CardContent className="flex justify-between items-center p-4">
+              <div>
+                <Skeleton className="h-6 w-32" />
+                <Skeleton className="h-4 w-24 mt-1" />
+              </div>
+              <Skeleton className="h-8 w-8" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Header({ router }: { router: ReturnType<typeof useRouter> }) {
+  return (
+    <div className="flex justify-between items-center w-full">
+      <Button variant="outline" size="icon" onClick={() => router.push(`/help`)}>
+        <CircleHelp />
+      </Button>
+      <h1 className="text-4xl font-bold">HoseJ</h1>
+      <Button variant="outline" size="icon" onClick={() => router.push(`/settings`)}>
+        <Settings />
+      </Button>
+    </div>
+  );
+}
+
+function Footer({ createGroup }: { createGroup: (groupName: string) => Promise<void> }) {
+  return (
     <div className="fixed bottom-0 left-0 w-full backdrop-blur-sm p-8 flex space-x-4">
-        <div className="w-1/2">
-          <CreateGroupDrawer onCreate={createGroup} />
-        </div>
-        <div className="w-1/2">
-          <JoinGroupDrawer />
-        </div>
+      <div className="w-1/2">
+        <CreateGroupDrawer onCreate={createGroup} />
+      </div>
+      <div className="w-1/2">
+        <JoinGroupDrawer />
       </div>
     </div>
   );
