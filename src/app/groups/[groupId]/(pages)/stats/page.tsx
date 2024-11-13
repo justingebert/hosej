0fb6080f01cf71705b540527040c655849de927d
Justin <justin.gebert.berlin@gmail.com>
2024-11-13 14:42:09 +0100

    refactor stats page

--- src/app/groups/[groupId]/(pages)/stats/page.tsx
+++ src/app/groups/[groupId]/(pages)/stats/page.tsx
@@ -7,64 +7,41 @@ import {
   TableCell,
   TableRow,
 } from "@/components/ui/table";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import SpinningLoader from "@/components/ui/custom/SpinningLoader";
 import Header from "@/components/ui/custom/Header";
 import { QuestionsByType, QuestionsByUser } from "@/components/Charts/QuestionCharts"; 
 import { Separator } from "@/components/ui/separator";
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
 
 type Statistics = {
   group :any
   userCount: number;
   questionsUsedCount: number;
   questionsLeftCount: number;
-  questionsByType: { _id: string; count: number }[]; // New field for questions by type
-  questionsByUser: { _id: string; count: number }[]; // New field for questions by user
+  questionsByType: { _id: string; count: number }[]; 
+  questionsByUser: { _id: string; count: number }[];
   messagesCount: number;
   RalliesUsedCount: number;
   RalliesLeftCount: number;
 };
 
-const fetchStatistics = async (groupId: string): Promise<Statistics> => {
-  const response = await fetch(`/api/groups/${groupId}/stats`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch statistics");
-  }
-  return response.json();
-};
 
 const StatsPage = () => {
-  const [loading, setLoading] = useState(true);
-  const [stats, setStats] = useState<Statistics | null>(null);
-  const [error, setError] = useState<string | null>(null);
   const { groupId } = useParams<{ groupId: string }>();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const statsData = await fetchStatistics(groupId);
-        setStats(statsData);
-      } catch (error: any) {
-        setError(error.message);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [groupId]);
+  const { data: stats, isLoading } = useSWR<Statistics>(`/api/groups/${groupId}/stats`, fetcher);
 
-  if (error) {
-    return <p className="text-red-500">{error}</p>;
-  }
-
-  if (loading) return <SpinningLoader loading={true} />;
+  if (isLoading) return <SpinningLoader loading={true} />;
   if (!stats) return <p>No statistics available</p>;
 
   const chartDataQuestions = [
     { questionsUsedCount: stats.questionsUsedCount, questionsLeftCount: stats.questionsLeftCount },
   ];
 
-  const totalQuestions = chartDataQuestions[0].questionsUsedCount + chartDataQuestions[0].questionsLeftCount;
+  const totalQuestions =
+    chartDataQuestions[0].questionsUsedCount + chartDataQuestions[0].questionsLeftCount;
 
   const chartDataRallies = [
     { RalliesUsedCount: stats.RalliesUsedCount, RalliesLeftCount: stats.RalliesLeftCount },
@@ -72,31 +49,14 @@ const StatsPage = () => {
 
   const totalRallies = chartDataRallies[0].RalliesUsedCount + chartDataRallies[0].RalliesLeftCount;
 
+
   return (
     <div>
-      <Header href={`/groups/${groupId}/dashboard`} />
-  
-      {/* User Section */}
-      <h2 className="text-xl font-bold text-center">Users</h2>
-      <Table className="w-full max-w-md">
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">User Count</TableCell>
-            <TableCell>{stats.group.members.length}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-
+      <Header title="Statistics" />
       <Separator className="my-6" />
-  
-      {/* Questions Section */}
       <h2 className="text-xl font-bold mb-4 text-center">Questions</h2>
       <Table className="w-full max-w-md">
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">Questions per Run</TableCell>
-            <TableCell>{stats.group.questionCount}</TableCell>
-          </TableRow>
           <TableRow>
             <TableCell className="font-medium">Questions Used</TableCell>
             <TableCell>{stats.questionsUsedCount}</TableCell>
@@ -121,12 +81,8 @@ const StatsPage = () => {
       <Separator className="my-6" />
 
       <h2 className="text-xl font-bold text-center">Rallies</h2>
-      <Table className="w-full max-w-md mb-6">
+      <Table className="w-full max-w-md">
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">Rallies per Run</TableCell>
-            <TableCell>{stats.group.rallyCount}</TableCell>
-          </TableRow>
           <TableRow>
             <TableCell className="font-medium">Rally Gap in Days</TableCell>
             <TableCell>{stats.group.rallyGapDays}</TableCell>
@@ -145,6 +101,20 @@ const StatsPage = () => {
           </TableRow>
         </TableBody>
       </Table>
+
+      <Separator className="my-6" />
+
+      <h2 className="text-xl font-bold text-center">Users</h2>
+      <Table className="w-full max-w-md">
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">User Count</TableCell>
+            <TableCell>{stats.group.members.length}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      
   
       <Separator className="my-6" />
 
