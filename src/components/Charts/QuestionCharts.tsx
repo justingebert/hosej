d7bea2b472b7f6b31af02923e149d611eaf9e9ce
Justin Gebert <justin.gebert.berlin@gmail.com>
2025-01-11 20:28:59 +0100

    new chart colors

--- src/components/Charts/QuestionCharts.tsx
+++ src/components/Charts/QuestionCharts.tsx
@@ -18,7 +18,7 @@ import {
 } from "@/components/ui/chart";
 
 // Sample colors for pie chart
-const COLORS = ["hsl(var(--chart-1))", "hsl(var(--chart-2))", "hsl(var(--chart-3))", "hsl(var(--chart-4))", "hsl(var(--chart-5))"];
+const COLORS = ["hsl(var(--chart-1))", "hsl(var(--chart-2))", "hsl(var(--chart-3))", "hsl(var(--chart-4))", "hsl(var(--chart-5))", "hsl(var(--chart-6))", "hsl(var(--chart-7))", "hsl(var(--chart-8))", "hsl(var(--chart-9))", "hsl(var(--chart-10))"];
 
 // Chart for Questions by Type
 export function QuestionsByType({ data }: { data: any[] }) {
