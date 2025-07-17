a376f8dee43f4ade5c1d7dbdbe0d10832711bef4
Justin Gebert <justin.gebert.berlin@gmail.com>
2025-07-17 22:50:35 +0200

    add haptics to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import { useHaptic } from "use-haptic";
 
 import { cn } from "@/lib/utils"
 
@@ -42,10 +43,12 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    const { triggerHaptic } = useHaptic();
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={triggerHaptic}
         {...props}
       />
     )
