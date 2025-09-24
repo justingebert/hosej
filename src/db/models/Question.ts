25d50d340e9dedaede33beb1cc69db96625b2d2e
Justin Gebert <justin.gebert.berlin@gmail.com>
2025-09-24 22:22:33 +0200

    update question schema style

--- src/db/models/Question.ts
+++ src/db/models/Question.ts
@@ -7,36 +7,36 @@ const answerSchema = new mongoose.Schema<IAnswer>({
     time: { type: Date, default: Date.now },
 });
 
-
-const questionSchemaFields = {
-    groupId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Group",
-        required: true,
-    },
-    category: { type: String },
-    questionType: {
-        type: String,
-        enum: Object.values(QuestionType),
-        required: true,
-    },
-    question: { type: String, required: true },
-    image: { type: String, required: false },
-    options: { type: mongoose.Schema.Types.Mixed, required: false },
-    answers: [answerSchema],
-    rating: {
-        good: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-        ok: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-        bad: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    },
-    used: { type: Boolean, default: false },
-    active: { type: Boolean, default: false },
-    usedAt: { type: Date },
-    submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    createdAt: { type: Date, default: Date.now },
-}
-const questionSchema = new mongoose.Schema<IQuestion>(questionSchemaFields);
+const questionSchema = new mongoose.Schema<IQuestion>(
+    {
+        groupId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Group",
+            required: true,
+        },
+        category: { type: String },
+        questionType: {
+            type: String,
+            enum: Object.values(QuestionType),
+            required: true,
+        },
+        question: { type: String, required: true },
+        image: { type: String, required: false },
+        options: { type: mongoose.Schema.Types.Mixed, required: false },
+        answers: [answerSchema],
+        rating: {
+            good: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+            ok: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+            bad: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+        },
+        used: { type: Boolean, default: false },
+        active: { type: Boolean, default: false },
+        usedAt: { type: Date },
+        submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+        createdAt: { type: Date, default: Date.now },
+    }
+);
 
 questionSchema.index({ groupId: 1 });
 
