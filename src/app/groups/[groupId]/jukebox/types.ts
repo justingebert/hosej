c776a4abc01100ec29ac38f6a8f5b958b87994f0
Justin Gebert <justin.gebert.berlin@gmail.com>
2025-09-26 09:14:39 +0200

    refactor: update jukebox model to use related types and DTO

--- src/app/groups/[groupId]/jukebox/types.ts
+++ src/app/groups/[groupId]/jukebox/types.ts
@@ -1,18 +1,18 @@
-import { IJukeboxJson, IRating, IRatingJson, ISong, ISongJson } from "@/types/models/jukebox";
+import { JukeboxDTO, IRating, RatingDTO, ISong, SongDTO } from "@/types/models/jukebox";
 import { UserDTO } from "@/types/models/user";
 
-export interface IProcessedRating extends Omit<IRatingJson, 'userId'> {
+export interface IProcessedRating extends Omit<RatingDTO, 'userId'> {
     userId: UserDTO;
 }
 
-export interface IProcessedSong extends Omit<ISongJson, 'ratings' | 'submittedBy'> {
+export interface IProcessedSong extends Omit<SongDTO, 'ratings' | 'submittedBy'> {
     avgRating: number | null; // Calculated field
     userHasRated: boolean; // Whether the current user has rated this song
     submittedBy: UserDTO;
     ratings: IProcessedRating[];
 }
 
-export interface IJukeboxProcessed extends Omit<IJukeboxJson, 'songs'> {
+export interface IJukeboxProcessed extends Omit<JukeboxDTO, 'songs'> {
     userHasSubmitted: boolean;
     songs: IProcessedSong[];
 }
