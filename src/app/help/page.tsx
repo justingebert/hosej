48f0f8c0b6cd0558240f638832387ecff7a8cb76
Justin Gebert <justin.gebert.berlin@gmail.com>
2026-02-23 20:13:59 +0100

    update img path

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/ui/custom/Header";
-import cat from "/public/cat.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -9,7 +8,7 @@ export default function HelpPage() {
     return (
         <>
             <Header leftComponent={<BackLink href={`/groups`} />} title={"Help"} />
-            <Image src={cat} alt="cat" className="rounded-lg" />
+            <Image src={"/cat.jpg"} alt="cat" className="rounded-lg" />
             <p>This is a private app in development </p>
             <div className="flex flex-row justify-between my-6">
                 <Link href={"/terms"}>
